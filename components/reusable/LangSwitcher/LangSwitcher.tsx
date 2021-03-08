d9d4f659a9c6a19bc22f60c2e937a04656de41cb
polovkin <zimboroda@gmail.com>
2021-03-08 17:14:52 +0200

    feat(langswitcher): change to links

---
+++ components/reusable/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import style from './LangSwitcher.module.scss'
+
+const LangSwitcher = () => {
+  return (
+    <>
+      <div className={style.wrap}>
+        <Link href="/" locale={'ru'}>
+          <button className={style.item}>RU</button>
+        </Link>
+        <Link href="/" locale={'en'}>
+          <button className={style.item}>EN</button>
+        </Link>
+      </div>
+    </>
+  )
+}
+export default LangSwitcher
